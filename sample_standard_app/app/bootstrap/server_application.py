854c9d185176a35f470fefcf89ee2e76c24ce255
Jerry Z H <jerry.zzw@antgroup.com>
2024-04-24 16:19:10 +0800

    feat: Initial docs & sample-standard-app

---
+++ sample_standard_app/app/bootstrap/server_application.py
@@ -0,0 +1,24 @@
+# !/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+# @Time    : 2024/4/8 20:58
+# @Author  : jerry.zzw 
+# @Email   : jerry.zzw@antgroup.com
+# @FileName: server_application.py
+from agentuniverse.agent_serve.web.web_booster import start_web_server
+from agentuniverse.base.agentuniverse import AgentUniverse
+
+
+class ServerApplication:
+    """
+    Server application.
+    """
+
+    @classmethod
+    def start(cls):
+        AgentUniverse().start()
+        start_web_server()
+
+
+if __name__ == "__main__":
+    ServerApplication.start()
