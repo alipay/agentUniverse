954f4b9870963ee5fcfe4bec0857f893b6bad477
wangchongshi <wangchongshi.wcs@antgroup.com>
2024-04-30 11:59:13 +0800

    Feat: add openai model context length.

--- agentuniverse/llm/openai_llm.py
+++ agentuniverse/llm/openai_llm.py
@@ -26,12 +26,14 @@ OPENAI_MAX_CONTEXT_LENGTH = {
     "gpt-35-turbo": 4096,
     "gpt-35-turbo-16k": 16384,
     "gpt-3.5-turbo-1106": 16384,
+    "gpt-3.5-turbo-0125": 16384,
     "gpt-4-0314": 8192,
     "gpt-4": 8192,
     "gpt-4-32k": 32768,
-    "gpt-4-32k-0314": 32768,
+    "gpt-4-32k-0613": 32768,
     "gpt-4-0613": 8192,
     "gpt-4-1106-preview": 128000,
+    "gpt-4-turbo": 128000,
 }
 
 
