e2bacd4798481a3e701df66660b59a33c539c0b0
weizjajj <weizhongjie.wzj@antgroup.com>
2024-05-08 11:56:28 +0800

    add streaming support

--- tests/test_agentuniverse/unit/llm/test_llm.py
+++ tests/test_agentuniverse/unit/llm/test_llm.py
@@ -27,7 +27,7 @@ class LLMTest(unittest.TestCase):
                 "content": "hi, please introduce yourself",
             }
         ]
-        output = self.llm.call(messages=messages)
+        output = self.llm.call(messages=messages,streaming=False)
         print(output.__str__())
 
     def test_acall(self) -> None:
@@ -37,8 +37,36 @@ class LLMTest(unittest.TestCase):
                 "content": "hi, please introduce yourself",
             }
         ]
-        output = asyncio.run(self.llm.acall(messages=messages))
+        output = asyncio.run(self.llm.acall(messages=messages,streaming=False))
         print(output.__str__())
+
+    def test_call_stream(self):
+        messages = [
+            {
+                "role": "user",
+                "content": "hi, please introduce yourself",
+            }
+        ]
+        for chunk in self.llm.call(messages=messages, streaming=True):
+            print(chunk.text, end='')
+        print()
+
+    #
+    def test_acall_stream(self):
+        messages = [
+            {
+                "role": "user",
+                "content": "hi, please introduce yourself",
+            }
+        ]
+        asyncio.run(self.call_stream(messages=messages))
+
+    #
+    async def call_stream(self, messages: list):
+
+        async for chunk in await self.llm.acall(messages=messages, streaming=True):
+            print(chunk, end='')
+        print()
 
     def test_as_langchain(self):
         langchain_llm = self.llm.as_langchain()
