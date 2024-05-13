45a4dc3ae05490f98ddc295c189f9bd49d9fe021
fanen.lhy <fanen.lhy@antgroup.com>
2024-05-13 17:20:34 +0800

    Edit: Docs file name. Add: Support call agent service by grpc.

--- agentuniverse/agent_serve/web/rpc/agentuniverse_service_pb2_grpc.py
+++ agentuniverse/agent_serve/web/rpc/agentuniverse_service_pb2_grpc.py
@@ -2,7 +2,7 @@
 """Client and server classes corresponding to protobuf-defined services."""
 import grpc
 
-import agentuniverse_service_pb2 as agentuniverse__service__pb2
+from . import agentuniverse_service_pb2 as agentuniverse__service__pb2
 
 
 class AgentUniverseServiceStub(object):
