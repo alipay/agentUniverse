7a7084ead14c21d297e2efd047518f64dd3c8b79
wangchongshi <wangchongshi.wcs@antgroup.com>
2024-05-16 15:06:04 +0800

    improvement: change the name of parameters in the parse_input method of the agent.

--- agentuniverse/agent/default/executing_agent/executing_agent.py
+++ agentuniverse/agent/default/executing_agent/executing_agent.py
@@ -26,19 +26,19 @@ class ExecutingAgent(Agent):
         """Return the output keys of the Agent."""
         return ['executing_result']
 
-    def parse_input(self, input_object: InputObject, planner_input: dict) -> dict:
-        """Planner parameter parsing.
+    def parse_input(self, input_object: InputObject, agent_input: dict) -> dict:
+        """Agent parameter parsing.
 
         Args:
-            input_object(InputObject): agent parameter object
-            planner_input(dict): Planner input
+            input_object (InputObject): input parameters passed by the user.
+            agent_input (dict): agent input preparsed by the agent.
         Returns:
-            dict: Planner input
+            dict: agent input parsed from `input_object` by the user.
         """
-        planner_input['input'] = input_object.get_data('input')
-        planner_input['framework'] = input_object.get_data('planning_result').get_data('framework')
+        agent_input['input'] = input_object.get_data('input')
+        agent_input['framework'] = input_object.get_data('planning_result').get_data('framework')
         self.agent_model.profile.setdefault('prompt_version', 'default_executing_agent.cn')
-        return planner_input
+        return agent_input
 
     def parse_result(self, planner_result: dict) -> dict:
         """Planner result parser.
