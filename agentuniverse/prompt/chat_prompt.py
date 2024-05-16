b295c045c04ba111705ad55cb9a142ceacb7cb1d
wangchongshi <wangchongshi.wcs@antgroup.com>
2024-05-16 13:35:34 +0800

    feat: add base64 local image prompt in agentUniverse.

--- agentuniverse/prompt/chat_prompt.py
+++ agentuniverse/prompt/chat_prompt.py
@@ -5,8 +5,10 @@
 # @Author  : wangchongshi
 # @Email   : wangchongshi.wcs@antgroup.com
 # @FileName: chat_prompt.py
+import base64
 from typing import List
 import re
+from urllib.parse import urlparse
 
 from langchain_core.prompts import ChatPromptTemplate
 
@@ -15,6 +17,14 @@ from agentuniverse.agent.memory.message import Message
 from agentuniverse.base.util.prompt_util import generate_chat_template
 from agentuniverse.prompt.prompt import Prompt
 from agentuniverse.prompt.prompt_model import AgentPromptModel
+
+image_extensions = (
+    ".png",
+    ".jpg",
+    ".jpeg",
+    ".gif",
+    ".webp",
+)
 
 
 class ChatPrompt(Prompt):
@@ -58,5 +68,20 @@ class ChatPrompt(Prompt):
         """
         if image_urls:
             for image_url in image_urls:
-                content = [{"type": "image_url", "image_url": {"url": image_url}}]
-                self.messages.append(Message(type=ChatMessageEnum.HUMAN.value, content=content))
+                parsed_url = urlparse(image_url)
+                # Check if the URL is a valid HTTP or HTTPS URL.
+                if parsed_url.scheme in ["http", "https"]:
+                    content = [{"type": "image_url", "image_url": {"url": image_url}}]
+                    self.messages.append(Message(type=ChatMessageEnum.HUMAN.value, content=content))
+                # Check if the URL is a local file.
+                elif parsed_url.scheme == "file" or not parsed_url.scheme:
+                    if parsed_url.path.lower().endswith(image_extensions):
+                        with open(parsed_url.path, "rb") as image_file:
+                            base64_image = base64.b64encode(image_file.read()).decode(
+                                "utf-8",
+                            )
+                            extension = parsed_url.path.lower().split(".")[-1]
+                            mime_type = f"image/{extension}"
+                            content = [
+                                {"type": "image_url", "image_url": {"url": f"data:{mime_type};base64,{base64_image}"}}]
+                            self.messages.append(Message(type=ChatMessageEnum.HUMAN.value, content=content))
