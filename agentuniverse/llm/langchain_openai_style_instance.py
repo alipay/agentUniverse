847dbafc73e9fd17560c0f07658b0937a67486c5
weizjajj <weizhongjie.wzj@antgroup.com>
2024-05-21 18:24:56 +0800

    add llm_support

---
+++ agentuniverse/llm/langchain_openai_style_instance.py
@@ -0,0 +1,132 @@
+# !/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+# @Time    : 2024/5/21 13:52
+# @Author  : weizjajj
+# @Email   : weizhongjie.wzj@antgroup.com
+# @FileName: langchain_openai_style_instance.py
+
+from typing import Any, List, Optional, AsyncIterator
+
+from langchain.callbacks.manager import AsyncCallbackManagerForLLMRun, CallbackManagerForLLMRun
+from langchain.schema import BaseMessage, ChatResult
+from langchain_community.chat_models.openai import _convert_delta_to_message_chunk
+from langchain_core.language_models.chat_models import generate_from_stream, agenerate_from_stream
+from langchain_core.messages import AIMessageChunk
+from langchain_core.outputs import ChatGenerationChunk
+from langchain_community.chat_models import ChatOpenAI
+
+from agentuniverse.llm.llm import LLM
+
+
+class LangchainOpenAIStyleInstance(ChatOpenAI):
+    """Langchain OpenAI LLM wrapper."""
+
+    llm: Optional[LLM] = None
+
+    def __init__(self, llm: LLM):
+        """The __init__ method.
+
+        The agentUniverse LLM instance is passed to this class as an argument.
+        Convert the attributes of agentUniverse(aU) LLM instance to the LangchainOpenAI object for initialization
+
+        Args:
+            llm (LLM): the agentUniverse(aU) LLM instance.
+        """
+        init_params = dict()
+        init_params['model_name'] = llm.model_name if llm.model_name else 'gpt-3.5-turbo'
+        init_params['temperature'] = llm.temperature if llm.temperature else 0.7
+        init_params['request_timeout'] = llm.request_timeout
+        init_params['max_tokens'] = llm.max_tokens
+        init_params['max_retries'] = llm.max_retries if llm.max_retries else 2
+        init_params['streaming'] = llm.streaming if llm.streaming else False
+        init_params['openai_api_key'] = llm.api_key if llm.api_key else 'blank'
+        init_params['openai_organization'] = llm.organization
+        init_params['openai_api_base'] = llm.api_base
+        init_params['openai_proxy'] = llm.proxy
+        super().__init__(**init_params)
+        self.llm = llm
+
+    def _generate(
+            self,
+            messages: List[BaseMessage],
+            stop: Optional[List[str]] = None,
+            run_manager: Optional[CallbackManagerForLLMRun] = None,
+            stream: Optional[bool] = None,
+            **kwargs,
+    ) -> ChatResult:
+        """Run the Langchain OpenAI LLM."""
+        should_stream = stream if stream is not None else self.streaming
+        message_dicts, params = self._create_message_dicts(messages, stop)
+        params = {**params, **kwargs}
+        llm_output = self.llm.call(messages=message_dicts, **params)
+        if not should_stream:
+            return self._create_chat_result(llm_output.raw)
+        stream_iter = self.as_langchain_chunk(llm_output)
+        return generate_from_stream(stream_iter)
+
+    async def _agenerate(
+            self,
+            messages: List[BaseMessage],
+            stop: Optional[List[str]] = None,
+            run_manager: Optional[AsyncCallbackManagerForLLMRun] = None,
+            stream: Optional[bool] = None,
+            **kwargs: Any,
+    ) -> ChatResult:
+
+        """Asynchronously run the Langchain OpenAI LLM."""
+        should_stream = stream if stream is not None else self.streaming
+        message_dicts, params = self._create_message_dicts(messages, stop)
+        params = {**params, **kwargs}
+        llm_output = await self.llm.acall(messages=message_dicts, **params)
+        if not should_stream:
+            return self._create_chat_result(llm_output.raw)
+        stream_iter = self.as_langchain_achunk(llm_output)
+        return await agenerate_from_stream(stream_iter)
+
+    @staticmethod
+    def as_langchain_chunk(stream, run_manager=None):
+        default_chunk_class = AIMessageChunk
+        for llm_result in stream:
+            chunk = llm_result.raw
+            if not isinstance(chunk, dict):
+                chunk = chunk.dict()
+            if len(chunk["choices"]) == 0:
+                continue
+            choice = chunk["choices"][0]
+            chunk = _convert_delta_to_message_chunk(
+                choice["delta"], default_chunk_class
+            )
+            finish_reason = choice.get("finish_reason")
+            generation_info = (
+                dict(finish_reason=finish_reason) if finish_reason is not None else None
+            )
+            default_chunk_class = chunk.__class__
+            chunk = ChatGenerationChunk(message=chunk, generation_info=generation_info)
+            yield chunk
+            if run_manager:
+                run_manager.on_llm_new_token(chunk.text, chunk=chunk)
+
+    @staticmethod
+    async def as_langchain_achunk(stream_iterator: AsyncIterator, run_manager=None) \
+            -> AsyncIterator[ChatGenerationChunk]:
+        default_chunk_class = AIMessageChunk
+        async for llm_result in stream_iterator:
+            chunk = llm_result.raw
+            if not isinstance(chunk, dict):
+                chunk = chunk.dict()
+            if len(chunk["choices"]) == 0:
+                continue
+            choice = chunk["choices"][0]
+            chunk = _convert_delta_to_message_chunk(
+                choice["delta"], default_chunk_class
+            )
+            finish_reason = choice.get("finish_reason")
+            generation_info = (
+                dict(finish_reason=finish_reason) if finish_reason is not None else None
+            )
+            default_chunk_class = chunk.__class__
+            chunk = ChatGenerationChunk(message=chunk, generation_info=generation_info)
+            yield chunk
+            if run_manager:
+                await run_manager.on_llm_new_token(token=chunk.text, chunk=chunk)
---
+++ agentuniverse/llm/openai_style_llm.py
@@ -0,0 +1,176 @@
+# !/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+# @Time    : 2024/5/21 13:52
+# @Author  : weizjajj
+# @Email   : weizhongjie.wzj@antgroup.com
+# @FileName: openai_style_llm.py
+
+from typing import Any, Optional, AsyncIterator, Iterator, Union
+
+import httpx
+import openai
+import tiktoken
+from langchain_core.language_models.base import BaseLanguageModel
+from openai import OpenAI, AsyncOpenAI
+
+from agentuniverse.llm.langchain_openai_style_instance import LangchainOpenAIStyleInstance
+from agentuniverse.llm.llm import LLM, LLMOutput
+
+
+class OpenAIStyleLLM(LLM):
+    """
+    This is a wrapper around the OpenAI API that implements a chat interface for the LLM.
+    It uses the `chat` endpoint of the OpenAI API.
+    It also supports using the `completion` endpoint instead of the `chat` endpoint.
+    It supports both sync and async modes.
+    Attributes:
+        api_key (Optional[str]): The API key to use for authentication.
+        organization (Optional[str]): The organization ID to use for authentication.
+        api_base (Optional[str]): The base URL to use for the API requests.
+        proxy (Optional[str]): The proxy to use for the API requests.
+        client_args (Optional[dict]): Additional arguments to pass to the client.
+    """
+
+    api_key: Optional[str] = None
+    organization: Optional[str] = None
+    api_base: Optional[str] = None
+    proxy: Optional[str] = None
+    client_args: Optional[dict] = None
+    model_max_context_length: int = None
+
+    def _new_client(self):
+        """Initialize the openai client."""
+        return OpenAI(
+            api_key=self.api_key,
+            organization=self.organization,
+            base_url=self.api_base,
+            timeout=self.request_timeout,
+            max_retries=self.max_retries,
+            http_client=httpx.Client(proxy=self.openai_proxy) if self.proxy else None,
+            **(self.client_args or {}),
+        )
+
+    def _new_async_client(self):
+        """Initialize the openai async client."""
+        return AsyncOpenAI(
+            api_key=self.api_key,
+            organization=self.organization,
+            base_url=self.api_base,
+            timeout=self.request_timeout,
+            max_retries=self.max_retries,
+            http_client=httpx.AsyncClient(proxy=self.proxy) if self.proxy else None,
+            **(self.client_args or {}),
+        )
+
+    def call(self, messages: list, **kwargs: Any) -> Union[LLMOutput, Iterator[LLMOutput]]:
+        """Run the OpenAI LLM.
+
+        Args:
+            messages (list): The messages to send to the LLM.
+            **kwargs: Arbitrary keyword arguments.
+        """
+        streaming = kwargs.pop("streaming") if "streaming" in kwargs else self.streaming
+        self.client = self._new_client()
+        client = self.client
+        chat_completion = client.chat.completions.create(
+            messages=messages,
+            model=kwargs.pop('model', self.model_name),
+            temperature=kwargs.pop('temperature', self.temperature),
+            stream=kwargs.pop('stream', streaming),
+            max_tokens=kwargs.pop('max_tokens', self.max_tokens),
+            **kwargs,
+        )
+        if not streaming:
+            text = chat_completion.choices[0].message.content
+            return LLMOutput(text=text, raw=chat_completion.model_dump())
+        return self.generate_stream_result(chat_completion)
+
+    async def acall(self, messages: list, **kwargs: Any) -> Union[LLMOutput, AsyncIterator[LLMOutput]]:
+        """Asynchronously run the OpenAI LLM.
+
+        Args:
+            messages (list): The messages to send to the LLM.
+            **kwargs: Arbitrary keyword arguments.
+        """
+        streaming = kwargs.pop("streaming") if "streaming" in kwargs else self.streaming
+        self.async_client = self._new_async_client()
+        async_client = self.async_client
+        chat_completion = await async_client.chat.completions.create(
+            messages=messages,
+            model=kwargs.pop('model', self.model_name),
+            temperature=kwargs.pop('temperature', self.temperature),
+            stream=kwargs.pop('stream', streaming),
+            max_tokens=kwargs.pop('max_tokens', self.max_tokens),
+            **kwargs,
+        )
+        if not streaming:
+            text = chat_completion.choices[0].message.content
+            return LLMOutput(text=text, raw=chat_completion.model_dump())
+        return self.agenerate_stream_result(chat_completion)
+
+    def as_langchain(self) -> BaseLanguageModel:
+        """Convert the AgentUniverse(AU) openai llm class to the langchain openai llm class."""
+        return LangchainOpenAIStyleInstance(self)
+
+    def set_by_agent_model(self, **kwargs) -> None:
+        """ Assign values of parameters to the OpenAILLM model in the agent configuration."""
+        super().set_by_agent_model(**kwargs)
+        if 'api_key' in kwargs and kwargs['api_key']:
+            self.api_key = kwargs['api_key']
+        if 'api_base' in kwargs and kwargs['api_base']:
+            self.api_base = kwargs['api_base']
+        if 'proxy' in kwargs and kwargs['proxy']:
+            self.proxy = kwargs['proxy']
+        if 'client_args' in kwargs and kwargs['client_args']:
+            self.client_args = kwargs['client_args']
+        if 'model_max_context_length' in kwargs and kwargs['model_max_context_length']:
+            self.model_max_context_length = kwargs['model_max_context_length']
+
+    @staticmethod
+    def parse_result(chunk):
+        """Generate the result of the stream."""
+        chat_completion = chunk
+        if not isinstance(chunk, dict):
+            chunk = chunk.dict()
+        if len(chunk["choices"]) == 0:
+            return
+        choice = chunk["choices"][0]
+        message = choice.get("delta")
+        text = message.get("content")
+        if not text:
+            return
+        return LLMOutput(text=text, raw=chat_completion.model_dump())
+
+    @classmethod
+    def generate_stream_result(cls, stream: openai.Stream):
+        """Generate the result of the stream."""
+        for chunk in stream:
+            llm_output = cls.parse_result(chunk)
+            if llm_output:
+                yield llm_output
+
+    @classmethod
+    async def agenerate_stream_result(cls, stream: AsyncIterator) -> AsyncIterator[LLMOutput]:
+        """Generate the result of the stream."""
+        async for chunk in stream:
+            llm_output = cls.parse_result(chunk)
+            if llm_output:
+                yield llm_output
+
+    def get_num_tokens(self, text: str) -> int:
+        """Get the number of tokens present in the text.
+
+        Useful for checking if an input will fit in an openai model's context window.
+
+        Args:
+            text: The string input to tokenize.
+
+        Returns:
+            The integer number of tokens in the text.
+        """
+        try:
+            encoding = tiktoken.encoding_for_model(self.model_name)
+        except KeyError:
+            encoding = tiktoken.get_encoding("cl100k_base")
+        return len(encoding.encode(text))
