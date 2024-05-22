f4beba3e2bd14cc34087316ba459bb1b46ec02ac
wangchongshi <wangchongshi.wcs@antgroup.com>
2024-05-22 11:52:14 +0800

    improvement: change the prompt path in the base package of the agentUniverse.

--- agentuniverse/base/util/prompt/__init__.py
+++ agentuniverse/base/util/prompt/__init__.py
@@ -1,7 +1,7 @@
 # !/usr/bin/env python3
 # -*- coding:utf-8 -*-
 
-# @Time    : 2024/4/22 21:04
+# @Time    : 2024/5/20 16:23
 # @Author  : wangchongshi
 # @Email   : wangchongshi.wcs@antgroup.com
 # @FileName: __init__.py.py
