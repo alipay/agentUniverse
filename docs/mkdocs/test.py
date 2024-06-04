279d40adf6e3719d784076fac69b650249f0e4ff
Jerry Z H <jerry.zzw@antgroup.com>
2024-06-04 16:33:14 +0800

    docs: update doc gen

---
+++ docs/mkdocs/test.py
@@ -0,0 +1,11 @@
+# !/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+# @Time    : 2024/6/4 14:40
+# @Author  : jerry.zzw 
+# @Email   : jerry.zzw@antgroup.com
+# @FileName: test.md.py
+from docs.mkdocs.gen_doc.gen_ref_pages import run
+
+if __name__ == "__main__":
+    run()
