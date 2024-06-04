6c8b613badf88d6b208d697c19e63e7c05adb164
Jerry Z H <jerry.zzw@antgroup.com>
2024-06-04 14:11:55 +0800

    docs: update doc file

--- docs/mkdocs/gen_doc/gen_ref_pages.py
+++ docs/mkdocs/gen_doc/gen_ref_pages.py
@@ -23,25 +23,80 @@ package_list = [
 TUTORIAL = "[1] Tutorial"
 API_REFERENCE = "[2] API Reference"
 
-with mkdocs_gen_files.open(f"{TUTORIAL}/Index.md", "w") as nav_file:
-    with open('docs/guidebook/en/0_index.md', 'r', encoding='utf-8') as file:
-        markdown_text = file.read()
-        nav_file.writelines(markdown_text)
+doc_list_map = {
+    '1_1_Introduction.md': 'Introduction',
+    '1_2_Installation.md': 'Installation',
+    '1_3_Quick_Start.md': 'Quick Start',
+    '1_4_Application_Engineering_Structure_Explanation.md': 'Application Structure Explanation',
+    '2_2_Domain_Component_Principles.md': 'Domain Component Principles',
+    '2_2_1_Agent.md': 'Agent',
+    '2_2_1_Agent_Create_And_Use.md': 'Agent Create And Use',
+    '2_2_1_Agent_Related_Domain_Objects.md': 'Agent Related Domain Objects',
+    '2_2_2_LLM.md': 'LLM',
+    '2_2_2_LLM_component_define_and_usage.md': 'LLM Define And Use',
+    '2_2_2_LLM_Related_Domain_Objects.md': 'LLM Related Domain Objects',
+    '2_2_3_Tool.md': 'Tool',
+    '2_2_3_Tool_Create_And_Use.md': 'Tool Create And Use',
+    '2_2_3_Tool_Related_Domain_Objects.md': 'Tool Related Domain Objects',
+    '2_4_1_Service_Registration_and_Usage.md': 'Service Registration And Use',
+    '2_4_1_Service_Information_Storage.md': 'Service Information Storage',
+    '2_4_1_Web_Server.md': 'Web_Server',
+    '2_4_1_Web_Api.md': 'Web Api',
+    '2_6_Logging_Utils.md': 'Logging Utils',
+    '2_7_Framework_Context.md': 'Framework Context',
+    '3_1_2_0_List_Of_LLMs.md': 'List Of LLMs',
+    '3_1_2_BaiChuan_LLM_Use.md': 'BaiChuan LLM Use',
+    '3_1_2_Kimi_LLM_Use.md': 'Kimi LLM Use',
+    '3_1_2_OpenAI_LLM_Use.md': 'OpenAI LLM Use',
+    '3_1_2_Qwen_LLM_Use.md': 'Qwen LLM Use',
+    '3_1_2_WenXin_LLM_Use.md': 'WenXin LLM Use',
+    '3_2_1_gRPC.md': 'gRPC',
+    '3_2_4_Alibaba_Cloud_SLS.md': 'Alibaba Cloud SLS',
+    '5_1_1_Docker_Container_Deployment.md': 'Docker Container Deployment',
+    '5_1_2_K8S_Deployment.md': 'K8S Deployment',
+    '6_1_Contact_Us.md': 'Contact Us',
+}
 
-with mkdocs_gen_files.open(f"{TUTORIAL}/QuickStart.md", "w") as nav_file:
-    with open('docs/guidebook/en/1_3_Quick_Start.md', 'r', encoding='utf-8') as file:
-        markdown_text = file.read()
-        nav_file.writelines(markdown_text)
+doc_number_map = {
+    '1_1_Introduction.md': '1.1',
+    '1_2_Installation.md': '1.2',
+    '1_3_Quick_Start.md': '1.3',
+    '1_4_Application_Engineering_Structure_Explanation.md': '1.4',
+    '2_2_Domain_Component_Principles.md': '2.2',
+    '2_2_1_Agent.md': '2.2.1',
+    '2_2_1_Agent_Create_And_Use.md': '2.2.1.1',
+    '2_2_1_Agent_Related_Domain_Objects.md': '2.2.1.2',
+    '2_2_2_LLM.md': '2.2.2',
+    '2_2_2_LLM_component_define_and_usage.md': '2.2.2.1',
+    '2_2_2_LLM_Related_Domain_Objects.md': '2.2.2.2',
+    '2_2_3_Tool.md': '2.2.3',
+    '2_2_3_Tool_Create_And_Use.md': '2.2.3.1',
+    '2_2_3_Tool_Related_Domain_Objects.md': '2.2.3.2',
+    '2_4_1_Service_Registration_and_Usage.md': '2.4.1',
+    '2_4_1_Service_Information_Storage.md': '2.4.2',
+    '2_4_1_Web_Server.md': '2.4.3',
+    '2_4_1_Web_Api.md': '2.4.4',
+    '2_6_Logging_Utils.md': '2.6',
+    '2_7_Framework_Context.md': '2.7',
+    '3_1_2_0_List_Of_LLMs.md': '3.1.2',
+    '3_1_2_BaiChuan_LLM_Use.md': '3.1.2.1',
+    '3_1_2_Kimi_LLM_Use.md': '3.1.2.2',
+    '3_1_2_OpenAI_LLM_Use.md': '3.1.2.3',
+    '3_1_2_Qwen_LLM_Use.md': '3.1.2.4',
+    '3_1_2_WenXin_LLM_Use.md': '3.1.2.5',
+    '3_2_1_gRPC.md': '3.2.1',
+    '3_2_4_Alibaba_Cloud_SLS.md': '3.2.4',
+    '5_1_1_Docker_Container_Deployment.md': '5.1.1',
+    '5_1_2_K8S_Deployment.md': '5.1.2',
+    '6_1_Contact_Us.md': '6.1',
+}
 
-with mkdocs_gen_files.open(f"{TUTORIAL}/Installation.md", "w") as nav_file:
-    with open('docs/guidebook/en/1_2_Installation.md', 'r', encoding='utf-8') as file:
-        markdown_text = file.read()
-        nav_file.writelines(markdown_text)
-
-with mkdocs_gen_files.open(f"{TUTORIAL}/ApplicationStructureExplanation.md", "w") as nav_file:
-    with open('docs/guidebook/en/1_4_Application_Engineering_Structure_Explanation.md', 'r', encoding='utf-8') as file:
-        markdown_text = file.read()
-        nav_file.writelines(markdown_text)
+for key, value in doc_list_map.items():
+    with mkdocs_gen_files.open(f"{TUTORIAL}/{value}.md", "w") as nav_file:
+        with open(f'docs/guidebook/en/{key}', 'r', encoding='utf-8') as file:
+            markdown_text = file.read()
+            nav_file.writelines(f'# {doc_number_map.get(key)} {value}')
+            nav_file.writelines(markdown_text)
 
 for package_path in package_list:
     for path in sorted(Path(package_path).rglob("*.py")):
