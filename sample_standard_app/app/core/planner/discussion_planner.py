4ed263ae5ede77a34cc5ccc011dfd51199408e46
wangchongshi <wangchongshi.wcs@antgroup.com>
2024-06-07 15:30:06 +0800

    feat: add demo discussion agents in agentUniverse.

--- sample_standard_app/app/core/planner/discussion_planner.py
+++ sample_standard_app/app/core/planner/discussion_planner.py
@@ -5,12 +5,25 @@
 # @Author  : wangchongshi
 # @Email   : wangchongshi.wcs@antgroup.com
 # @FileName: discussion_planner.py
+import asyncio
+
+from langchain_core.chat_history import InMemoryChatMessageHistory
+from langchain_core.runnables.history import RunnableWithMessageHistory
+
 from agentuniverse.agent.agent_manager import AgentManager
 from agentuniverse.agent.agent_model import AgentModel
 from agentuniverse.agent.input_object import InputObject
+from agentuniverse.agent.memory.chat_memory import ChatMemory
 from agentuniverse.agent.output_object import OutputObject
 from agentuniverse.agent.plan.planner.planner import Planner
 from agentuniverse.base.util.logging.logging_util import LOGGER
+from agentuniverse.base.util.memory_util import generate_memories
+from agentuniverse.base.util.prompt_util import process_llm_token
+from agentuniverse.llm.llm import LLM
+from agentuniverse.prompt.chat_prompt import ChatPrompt
+from agentuniverse.prompt.prompt import Prompt
+from agentuniverse.prompt.prompt_manager import PromptManager
+from agentuniverse.prompt.prompt_model import AgentPromptModel
 
 default_round = 2
 
@@ -30,7 +43,7 @@ class DiscussionPlanner(Planner):
         """
         planner_config = agent_model.plan.get('planner')
         participant_agents = self.generate_participant_agents(planner_config)
-        return self.agents_run(participant_agents, planner_config, planner_input, input_object)
+        return self.agents_run(participant_agents, planner_config, agent_model, planner_input, input_object)
 
     @staticmethod
     def generate_participant_agents(planner_config: dict) -> dict:
@@ -43,8 +56,8 @@ class DiscussionPlanner(Planner):
             agents[participant_name] = AgentManager().get_instance_obj(participant_name)
         return agents
 
-    @staticmethod
-    def agents_run(agents: dict, planner_config: dict, agent_input: dict, input_object: InputObject) -> dict:
+    def agents_run(self, agents: dict, planner_config: dict, agent_model: AgentModel, agent_input: dict,
+                   input_object: InputObject) -> dict:
         result: dict = dict()
         total_round: int = planner_config.get('round', default_round)
         chat_history = []
@@ -68,4 +81,64 @@ class DiscussionPlanner(Planner):
                 # get the result
                 result = output_object.to_dict()
                 LOGGER.info(f"the {i + 1} round agent {agent_name} thought: {output_object.get_data('output', '')}.")
-        return result
+
+        agent_input['chat_history'] = chat_history
+        return self.invoke_host_agent(agent_model, agent_input, input_object)
+
+    def invoke_host_agent(self, agent_model: AgentModel, planner_input: dict, input_object: InputObject) -> dict:
+        LOGGER.info(f"Discussion end.")
+        LOGGER.info(f"Host agent starts summarize the discussion.")
+        memory: ChatMemory = self.handle_memory(agent_model, planner_input)
+
+        llm: LLM = self.handle_llm(agent_model)
+
+        prompt: ChatPrompt = self.handle_prompt(agent_model, planner_input)
+        process_llm_token(llm, prompt.as_langchain(), agent_model.profile, planner_input)
+
+        chat_history = memory.as_langchain().chat_memory if memory else InMemoryChatMessageHistory()
+
+        chain_with_history = RunnableWithMessageHistory(
+            prompt.as_langchain() | llm.as_langchain(),
+            lambda session_id: chat_history,
+            history_messages_key="chat_history",
+            input_messages_key=self.input_key,
+        )
+        res = asyncio.run(
+            chain_with_history.ainvoke(input=planner_input, config={"configurable": {"session_id": "unused"}}))
+        LOGGER.info(f"Discussion summary is: {res.content}.")
+        return {**planner_input, self.output_key: res.content, 'chat_history': generate_memories(chat_history)}
+
+    def handle_prompt(self, agent_model: AgentModel, planner_input: dict) -> ChatPrompt:
+        """Prompt module processing.
+
+        Args:
+            agent_model (AgentModel): Agent model object.
+            planner_input (dict): Planner input object.
+        Returns:
+            ChatPrompt: The chat prompt instance.
+        """
+        profile: dict = agent_model.profile
+
+        profile_prompt_model: AgentPromptModel = AgentPromptModel(introduction=profile.get('introduction'),
+                                                                  target=profile.get('target'),
+                                                                  instruction=profile.get('instruction'))
+
+        # get the prompt by the prompt version
+        prompt_version: str = profile.get('prompt_version')
+        version_prompt: Prompt = PromptManager().get_instance_obj(prompt_version)
+
+        if version_prompt is None and not profile_prompt_model:
+            raise Exception("Either the `prompt_version` or `introduction & target & instruction`"
+                            " in agent profile configuration should be provided.")
+        if version_prompt:
+            version_prompt_model: AgentPromptModel = AgentPromptModel(
+                introduction=getattr(version_prompt, 'introduction', ''),
+                target=getattr(version_prompt, 'target', ''),
+                instruction=getattr(version_prompt, 'instruction', ''))
+            profile_prompt_model = profile_prompt_model + version_prompt_model
+
+        chat_prompt = ChatPrompt().build_prompt(profile_prompt_model, self.prompt_assemble_order)
+        image_urls: list = planner_input.pop('image_urls', []) or []
+        if image_urls:
+            chat_prompt.generate_image_prompt(image_urls)
+        return chat_prompt
