85d01c6f00218aa9c4a86bd4c9b6565a357b7a64
Jerry Z H <jerry.zzw@antgroup.com>
2024-06-22 12:49:42 +0800

    Removed: move dataflow to sepc branch

--- agentuniverse_dataflow/node/data/prompt/__init__.py
+++
@@ -1,7 +0,0 @@
-# !/usr/bin/env python3
-# -*- coding:utf-8 -*-
-
-# @Time    : 2024/6/17 14:54
-# @Author  : wangchongshi
-# @Email   : wangchongshi.wcs@antgroup.com
-# @FileName: __init__.py.py
--- agentuniverse_dataflow/node/model/__init__.py
+++
@@ -1,7 +0,0 @@
-# !/usr/bin/env python3
-# -*- coding:utf-8 -*-
-
-# @Time    : 2024/6/14 17:53
-# @Author  : wangchongshi
-# @Email   : wangchongshi.wcs@antgroup.com
-# @FileName: __init__.py.py
