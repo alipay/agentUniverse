844c62f67397bc05326c9082adc97186f956935d
weizjajj <weizhongjie.wzj@antgroup.com>
2024-08-22 09:47:16 +0800

    fix sh

--- sample_standard_app/quick_start.sh
+++ sample_standard_app/quick_start.sh
@@ -1,58 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
-# Ignore broken pipe errors
-trap '' PIPE
-
-# Check if Python is installed, if not, install it ，或者python的版本小于3.10
-if ! command -v python &> /dev/null; then
-    echo "Python is not installed. Installing Python..."
-    # Check if conda is installed
-    if ! command -v conda &> /dev/null; then
-        echo "conda is not installed. Installing conda..."
-        mkdir -p ~/miniconda3
-        # If on macOS
-        # 根据cpu类型与系统类型mac,linux，x86_64,arm，下载
-        if [[ "$(uname)" == "Darwin" ]]; then
-            if [[ "$(uname -m)" == "x86_64" ]]; then
-                curl -o ~/miniconda3/miniconda.sh https://mirrors.tuna.tsinghua.edu.cn/anaconda/miniconda/Miniconda3-py39_24.3.0-0-MacOSX-x86_64.sh
-            elif [[ "$(uname -m)" == "arm64" ]];then
-                curl -o ~/miniconda3/miniconda.sh https://mirrors.tuna.tsinghua.edu.cn/anaconda/miniconda/Miniconda3-py39_24.3.0-0-MacOSX-arm64.sh
-            fi
-        # If on Linux
-        elif [[ "$(uname)" == "Linux" ]]; then
-            if [[ "$(uname -m)" == "x86_64" ]]; then
-                curl -o ~/miniconda3/miniconda.sh https://mirrors.tuna.tsinghua.edu.cn/anaconda/miniconda/Miniconda3-py39_24.3.0-0-Linux-x86_64.sh
-              fi
-            if [[ "$(uname -m)" == "aarch64" ]];then
-                    curl -o ~/miniconda3/miniconda.sh https://mirrors.tuna.tsinghua.edu.cn/anaconda/miniconda/Miniconda3-py39_24.3.0-0-Linux-aarch64.sh
-              fi
-        fi
-        bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
-        $HOME/miniconda3/bin/conda init bash
-        $HOME/miniconda3/bin/conda init sh
-        # 配置conda环境变量
-        echo 'export PATH="$HOME/miniconda3/bin:$PATH"' >> ~/.bashrc
-        source ~/.bashrc
-        export PATH="$HOME/miniconda3/bin:$PATH"
-        rm -rf ~/miniconda3/miniconda.sh
-        # Configure Tsinghua mirrors
-        conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/free/
-        conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/main/
-        conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud/conda-forge/
-        conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud/pytorch/
-    fi
-    # Create and activate Python 3.10 environment
-    conda create -n python_au3.10 python=3.10 -y
-    conda config --set default_env python_au3.10
-    python --version
-    conda init
-    conda activate python_au3.10 && python --version
+# 判断当前的python版本是否是3.10
+if ! python --version | grep -q "3.10"; then
+    echo "Error: Python version is not 3.10. Please install Python 3.10 and try again."
+    exit 1
 fi
 
-# Install missing packages
-if ! conda activate python_au3.10 && pip list | grep -q "agentUniverse"; then
-    echo "agentUniverse is not installed. Installing agentUniverse..."
-    conda activate python_au3.10 && pip install agentUniverse
+# 判断 pip list 是否已安装 agentUniverse
+if ! pip list | grep -q "agentUniverse"; then
+   pip install agentUniverse
 fi
 
 # Check if custom_key file exists, if not, copy from sample
@@ -65,4 +21,4 @@ export PYTHONPATH=$PWD/../..
 
 # Start the service
 cd app/bootstrap
-conda activate python_au3.10 && python -u server_application.py
+python -u server_application.py
