541d2b9a970e4ee81246328b2072354a57ed6a90
sunshinesmilelk <1176136681@qq.com>
2024-08-27 20:16:17 +0800

    feat: add parse_openapi_yaml_to_tool_bundle utils

--- agentuniverse_product/service/util/plugin_util.py
+++ agentuniverse_product/service/util/plugin_util.py
@@ -1,4 +1,8 @@
+import re
 from typing import Dict
+import uuid
+
+from yaml import safe_load
 from agentuniverse_product.base.product_manager import ProductManager
 from agentuniverse_product.service.model.plugin_dto import PluginDTO
 
@@ -26,4 +30,76 @@ def assemble_plugin_product_config_data(plugin_dto: PluginDTO) -> Dict:
         },
         'toolset': ['google_search_tool']
     }
+
+def parse_openapi_yaml_to_tool_bundle(yaml: str) -> list:
+    """
+        parse openapi yaml to tool bundle
+
+        :param yaml: the yaml string
+        :return: the tool bundle
+    """
+
+    openapi: dict = safe_load(yaml)
+    if openapi is None:
+        raise Exception('Invalid openapi yaml.')
+
+
+    if len(openapi['servers']) == 0:
+        raise Exception('No server found in the openapi yaml.')
+
+    server_url = str(openapi['servers'][0]['url'])
+
+    # list all interfaces
+    interfaces = []
+    for path, path_item in openapi['paths'].items():
+        methods = ['get', 'post', 'put', 'delete',
+                    'patch', 'head', 'options', 'trace']
+        for method in methods:
+            if method in path_item:
+                interfaces.append({
+                    'path': path,
+                    'method': method,
+                    'operation': path_item[method],
+                    'url': server_url,
+                })
+
+    for interface in interfaces:
+        # convert parameters
+        parameters = []
+        if 'parameters' in interface['operation']:
+            for parameter in interface['operation']['parameters']:
+                parameters.append(parameter)
+        # create tool bundle
+        # check if there is a request body
+        if 'requestBody' in interface['operation']:
+            request_body = interface['operation']['requestBody']
+            if 'content' in request_body:
+                for content_type, content in request_body['content'].items():
+                    # if there is a reference, get the reference and overwrite the content
+                    if 'schema' not in content:
+                        continue
+
+                    if '$ref' in content['schema']:
+                        # get the reference
+                        root = openapi
+                        reference = content['schema']['$ref'].split(
+                            '/')[1:]
+                        for ref in reference:
+                            root = root[ref]
+                        # overwrite the content
+                        interface['operation']['requestBody']['content'][content_type]['schema'] = root
+
+        if 'operationId' not in interface['operation']:
+            # remove special characters like / to ensure the operation id is valid ^[a-zA-Z0-9_-]{1,64}$
+            path = interface['path']
+            if interface['path'].startswith('/'):
+                path = interface['path'][1:]
+            # remove special characters like / to ensure the operation id is valid ^[a-zA-Z0-9_-]{1,64}$
+            path = re.sub(r'[^a-zA-Z0-9_-]', '', path)
+            if not path:
+                path = str(uuid.uuid4())
+
+            interface['operation']['operationId'] = f'{path}_{interface["method"]}'
+
+    return interfaces
 
---
+++ sample_standard_app/app/test/test_utils.py
@@ -0,0 +1,239 @@
+import unittest
+
+from agentuniverse_product.service.util.plugin_util import parse_openapi_yaml_to_tool_bundle
+
+
+arxiv = """
+info:
+    description: arxiv查找论文
+    title: arxiv_app
+    version: v1
+openapi: 3.0.1
+paths:
+    /arxiv:
+        get:
+            operationId: search
+            parameters:
+                - description: 输入搜索关键词，必须是英文
+                  in: query
+                  name: search_query
+                  required: true
+                  schema:
+                    type: string
+                - description: 搜索数量
+                  in: query
+                  name: count
+                  schema:
+                    type: integer
+                - description: 可以是1:降序2:升序
+                  in: query
+                  name: sort_order
+                  schema:
+                    type: integer
+                - description: 可以是1:相关性2.lastUpdateDate3.submittedDate
+                  in: query
+                  name: sort_by
+                  schema:
+                    type: integer
+                - description: 报告编号过滤器，用于获取报告编号中包含给定关键字的结果
+                  in: query
+                  name: report_number
+                  schema:
+                    type: string
+                - description: 主题类别过滤器，用于获取主题类别中包含给定关键字的结果。
+                  in: query
+                  name: subject_category
+                  schema:
+                    type: string
+                - description: 标题过滤器，用于获取标题包含给定关键字的结果
+                  in: query
+                  name: title
+                  schema:
+                    type: string
+                - description: 摘要过滤器，用于获取摘要中包含给定关键字的结果
+                  in: query
+                  name: abstract
+                  schema:
+                    type: string
+                - description: 作者过滤器，用于获取包含给定关键字的作者的结果
+                  in: query
+                  name: author
+                  schema:
+                    type: string
+                - description: 评论过滤器，用于获取评论中包含给定关键字的结果
+                  in: query
+                  name: comment
+                  schema:
+                    type: string
+                - description: 期刊参考过滤器，用于获取期刊参考中包含给定关键字的结果
+                  in: query
+                  name: journal_reference
+                  schema:
+                    type: string
+            requestBody:
+                content:
+                    application/json:
+                        schema:
+                            type: object
+            responses:
+                "200":
+                    content:
+                        application/json:
+                            schema:
+                                properties:
+                                    items:
+                                        items:
+                                            properties:
+                                                authors:
+                                                    description: 作者列表
+                                                    items:
+                                                        description: 作者
+                                                        type: string
+                                                    type: array
+                                                entry_id:
+                                                    description: https://arxiv.org/abs/{id} 链接
+                                                    type: string
+                                                pdf_url:
+                                                    description: pdf 下载链接
+                                                    type: string
+                                                published:
+                                                    description: 发表时间
+                                                    type: string
+                                                summary:
+                                                    description: 摘要
+                                                    type: string
+                                                title:
+                                                    description: 标题
+                                                    type: string
+                                            type: object
+                                        type: array
+                                    total:
+                                        description: 结果数目
+                                        type: integer
+                                type: object
+                    description: new desc
+                default:
+                    description: ""
+            summary: search
+    /test:
+        get:
+            operationId: search
+            parameters:
+                - description: 输入搜索关键词，必须是英文
+                  in: query
+                  name: search_query
+                  required: true
+                  schema:
+                    type: string
+                - description: 搜索数量
+                  in: query
+                  name: count
+                  schema:
+                    type: integer
+                - description: 可以是1:降序2:升序
+                  in: query
+                  name: sort_order
+                  schema:
+                    type: integer
+                - description: 可以是1:相关性2.lastUpdateDate3.submittedDate
+                  in: query
+                  name: sort_by
+                  schema:
+                    type: integer
+                - description: 报告编号过滤器，用于获取报告编号中包含给定关键字的结果
+                  in: query
+                  name: report_number
+                  schema:
+                    type: string
+                - description: 主题类别过滤器，用于获取主题类别中包含给定关键字的结果。
+                  in: query
+                  name: subject_category
+                  schema:
+                    type: string
+                - description: 标题过滤器，用于获取标题包含给定关键字的结果
+                  in: query
+                  name: title
+                  schema:
+                    type: string
+                - description: 摘要过滤器，用于获取摘要中包含给定关键字的结果
+                  in: query
+                  name: abstract
+                  schema:
+                    type: string
+                - description: 作者过滤器，用于获取包含给定关键字的作者的结果
+                  in: query
+                  name: author
+                  schema:
+                    type: string
+                - description: 评论过滤器，用于获取评论中包含给定关键字的结果
+                  in: query
+                  name: comment
+                  schema:
+                    type: string
+                - description: 期刊参考过滤器，用于获取期刊参考中包含给定关键字的结果
+                  in: query
+                  name: journal_reference
+                  schema:
+                    type: string
+            requestBody:
+                content:
+                    application/json:
+                        schema:
+                            type: object
+            responses:
+                "200":
+                    content:
+                        application/json:
+                            schema:
+                                properties:
+                                    items:
+                                        items:
+                                            properties:
+                                                authors:
+                                                    description: 作者列表
+                                                    items:
+                                                        description: 作者
+                                                        type: string
+                                                    type: array
+                                                entry_id:
+                                                    description: https://arxiv.org/abs/{id} 链接
+                                                    type: string
+                                                pdf_url:
+                                                    description: pdf 下载链接
+                                                    type: string
+                                                published:
+                                                    description: 发表时间
+                                                    type: string
+                                                summary:
+                                                    description: 摘要
+                                                    type: string
+                                                title:
+                                                    description: 标题
+                                                    type: string
+                                            type: object
+                                        type: array
+                                    total:
+                                        description: 结果数目
+                                        type: integer
+                                type: object
+                    description: new desc
+                default:
+                    description: ""
+            summary: search
+servers:
+    - url: http://exp-tech.plugin.knowledgepie.cn
+"""
+
+class UtilsTest(unittest.TestCase):
+    """
+    Test cases for the SQLDBWrapper
+    """
+
+    def setUp(self) -> None:
+        pass
+
+    def test_util(self):
+        print('parse_openapi_yaml_to_tool_bundle',parse_openapi_yaml_to_tool_bundle(arxiv))
+
+if __name__ == '__main__':
+    unittest.main()
